1570d904463e69c1bccc105dd511a242a3cad6e1
Tzivony <thsivony.itay@gmail.com>
2024-07-15 13:18:29 +0300

    Added avalon_sampler logic

--- rtl/avalon_sampler.sv
+++ rtl/avalon_sampler.sv
@@ -26,13 +26,67 @@ If this is intentional, set parameter SUPPRESS_WARNING to '1'");
 		end
 
 		if (CAPACITY > 16) begin
-			$warning("For larger data capacities, it is advised you use a desicated FIFO module.\n
+			$warning("For larger data capacities, it is advised you use a dedicated FIFO module.\n
 If this is intentional, set parameter SUPPRESS_WARNING to '1'");
 		end
 	end
 
+	// Constants
+	localparam int PAYLOAD_WIDTH = msg_in.DATA_WIDTH + msg_in.META_WIDTH;
+
+	// Declerations
+	logic [PAYLOAD_WIDTH-1:0] payload_in;
+	logic [PAYLOAD_WIDTH-1:0] payload_out;
+
+	logic [CAPACITY:0] one_hot;
+	logic              in_tran, out_tran;
+
+	logic [CAPACITY-1:0] load_en;
+
+	// Logic
+	begin : conc
+		assign payload_in = {msg_in.data, msg_in.empty, msg_in.sop, msg_in.eop};
+		assign {msg_out.data, msg_out.empty, msg_out.sop, msg_out.eop} = payload_out;
+	end
+
+	begin : onehot_sm
+		assign in_tran  = msg_in.vld & msg_in.rdy;
+		assign out_tran = msg_out.vld & msg_out.rdy;
+
+		_one_hot #(
+			.ONEHOT_WIDTH(CAPACITY + 1),
+			.POLARITY    (1'b0        )
+		) i_one_hot (
+			.clk     (clk     ),
+			.rst_n   (rst_n   ),
+			.inc     (in_tran ),
+			.dec     (out_tran),
+			.data_out(one_hot )
+		);
+
+		assign msg_in.rdy  = one_hot[CAPACITY];
+		assign msg_out.vld = one_hot[0];
+	end
+
+	begin : payload_sr		
+		assign load_en = {CAPACITY{in_tran}} & (~one_hot >> out_tran); // As we actually want to write into the previous index if reading occours
+
+		generate
+			for (int i = 0; i < PAYLOAD_WIDTH; i++) begin
+				_shift_reg #(.REG_WIDTH(CAPACITY)) i_shift_reg (
+					.clk      (clk           ),
+					.rst_n    (rst_n         ),
+					.load_data(payload_in[i] ),
+					.load_en  (load_en       ),
+					.sh       (out_tran      ),
+					.data_out (payload_out[i])    // This should take only the LSB bit of the shift-register
+				);
+			end
+		endgenerate
+	end
 
 endmodule : avalon_sampler
+
 
 module _shift_reg #(
 	parameter unsigned int REG_WIDTH
@@ -70,9 +124,10 @@ module _shift_reg #(
 
 endmodule : _shift_reg
 
+
 module _one_hot #(
 	parameter unsigned int ONEHOT_WIDTH,
-	parameter bit POLARITY = 1'b1
+	parameter bit POLARITY = 1'b1 // i.e. one-hot shifts around a '1' value by default
 ) (
 	input logic clk,    // Clock
 	input logic rst_n,  // Asynchronous reset active low
