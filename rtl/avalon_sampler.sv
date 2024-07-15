474b834cf5eb7038340035ce933824e16cca4107
Tzivony <thsivony.itay@gmail.com>
2024-07-15 12:12:05 +0300

    Added reqired shift-register

--- rtl/avalon_sampler.sv
+++ rtl/avalon_sampler.sv
@@ -33,3 +33,39 @@ If this is intentional, set parameter SUPPRESS_WARNING to '1'");
 
 
 endmodule : avalon_sampler
+
+module _shift_reg #(
+	parameter unsigned int REG_WIDTH
+) (
+	input logic clk,    // Clock
+	input logic rst_n,  // Asynchronous reset active low
+	
+	input logic load_data,
+	input logic [REG_WIDTH-1:0] load_en, // One-hot index
+	input logic sh,
+
+	output logic [REG_WIDTH:0] data_out  // Extra const-0 bit at the end (can be ignored)
+);
+	
+	always_ff @(posedge clk or negedge rst_n) begin
+		if(~rst_n) begin
+			data_out[REG_WIDTH] <= 1'b0;
+	end
+
+	generate
+		for (int i = 0; i < REG_WIDTH; i++) begin
+			always_ff @(posedge clk or negedge rst_n) begin
+				if(~rst_n) begin
+					data_out[i] <= 1'b0;
+				end else begin
+					if (load_en[i]) begin
+						data_out[i] <= load_data;
+					end else if (sh) begin
+						data_out[i] <= data_out[i+1];
+					end
+				end
+			end			
+		end
+	endgenerate
+
+endmodule : _shift_reg
